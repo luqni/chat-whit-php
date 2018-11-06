149831020ea33aa2a1049e94129b60a43d127aa3
Muhammad Luqni Baehaqi <mluqnibaehaqi@gmail.com>
2018-11-06 23:35:36 +0700

    first commit

---
+++ proses.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Aplikasi Chat Sederhana Hakko Blog's</title>
+</head>
+<body bgcolor="#45B5FF">
+<?php 
+
+$hr=array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
+$hari_ini=date("w");
+$hari_ini=$hr[$hari_ini];
+$jam = date ("H:i:s");
+
+$tgl=date("d/m/Y");
+$aktif="1";
+$nama = $_POST["nama"];
+$email = $_POST["email"];
+$komentar = $_POST["komentar"];
+
+
+$fp = fopen("guestbook.txt","a+");
+fputs($fp,"$hari_ini|$tgl|$jam|$nama|$email|$komentar\n");
+fclose($fp);
+echo("<script>alert('Komentar anda telah berhasil ditambahkan.'); window.location = 'komentar.php'</script>");
+
+
+?>
+</body>
+</html>
